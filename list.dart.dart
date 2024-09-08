0214ace8a3633ab11d4bd5bf7a0d76a9750ae990
Hassansharpdata <sharpdata.hassan@gmail.com>
2024-09-07 19:15:00 -0700

    Assignment Dart

---
+++ list.dart.dart
@@ -0,0 +1,53 @@
+void main(){
+
+// Data Types: List 3rd Class
+
+// List mylist = ["abcd", 123, "efgh", 456];
+// print(mylist[2]);
+
+// List mylist = ["abcd", 123, "efgh", 456];
+// print("Before: $mylist");
+// mylist[2] =  "asd";
+// print(mylist);
+
+// List mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// print("Before replacment: $mylist");
+// mylist[9] =  "abc";
+
+// Replaced list
+// mylist.replaceRange(1, 3, [7]);
+// print("After replacment: $mylist");
+
+
+// List mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// print("Before replacment: $mylist");
+// // mylist.insert(1, [6, 7]);
+// mylist.insertAll(5, [12, 13, 14, 15]);
+// // sorting list
+// mylist.sort();
+// var list = List.of(mylist.reversed);
+// print("After reversed: $list");
+
+
+// // REMOVE METHOD
+// List mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// mylist.remove(10);
+// print("After remove: $mylist");
+
+
+// Practice
+// List mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// mylist.remove(10);
+// print("After remove: $mylist");
+
+// mylist.sort();
+// var revrsd = List.of(mylist.reversed);
+// print("After reversed: $revrsd");
+
+// List inlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// inlist.insert(1,  [9, 10]);
+// print("After insert: $inlist");
+
+
+
+}
